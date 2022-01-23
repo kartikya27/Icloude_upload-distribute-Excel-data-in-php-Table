b5dac8d398ce94e28aba65483df70695191c3f8c
Kartik Mandothiya <kartikya27@gmail.com>
2022-01-23 18:00:00 +0530

    basic

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,6 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('index');
 	}
 }
--- application/views/index.php
+++ application/views/index.php
@@ -74,7 +74,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code><input type="file"></code>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
